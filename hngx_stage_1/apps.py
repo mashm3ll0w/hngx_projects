6d7dec332a6498a61c63844dcaa01b4e3ad748b6
Charles Swaleh <charlswall@live.com>
2023-09-09 19:23:34 +0300

    initial commit

---
+++ hngx_stage_1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HngxStage1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hngx_stage_1'
---
+++ hngx_stage_1/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import datetime
+
+# Create your views here.
+
+def index(request):
+  if request.method == "GET":
+    param_username = request.GET['slack_name']
+    param_track = request.GET['track']
+    slack_name = "Charles Swaleh"
+
+    if param_username.lower() == slack_name.lower() and param_track.lower() == "backend":
+      data = {
+      "slack_name": "Charles Swaleh",
+      "current_day": datetime.datetime.today().strftime('%A'),
+      "utc_time": datetime.datetime.now(datetime.timezone.utc),
+      "track": "backend",
+      "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
+      "github_repo_url": "https://github.com/username/repo",
+      "status_code": HttpResponse.status_code
+    }
+      return JsonResponse(data, safe=False)
+    else:
+      return JsonResponse({"Error": "Wrong User/Track"}, safe=False)
+  else:
+    return JsonResponse({"Error": "Please use a GET request"}, safe=False)
