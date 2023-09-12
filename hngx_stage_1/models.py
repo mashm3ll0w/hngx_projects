0916d5feb4e538525cdc11803e27580bfda626bd
Charles Swaleh <charlswall@live.com>
2023-09-12 13:13:58 +0300

    add the person model

--- hngx_stage_1/models.py
+++ hngx_stage_1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+  name = models.CharField(max_length=64)
+
+  def serialize(self):
+    return {
+      "id": self.id,
+      "name": self.name
+    }
--- hngx_stage_1/views.py
+++ hngx_stage_1/views.py
@@ -1,27 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-import datetime
-
+from .models import Person
 # Create your views here.
 
 def index(request):
-  if request.method == "GET":
-    param_username = request.GET['slack_name']
-    param_track = request.GET['track']
-    slack_name = "Charles Swaleh"
-
-    if param_username.lower() == "swaleh" and param_track.lower() == "backend":
-      data = {
-      "slack_name": "Charles Swaleh",
-      "current_day": datetime.datetime.today().strftime('%A'),
-      "utc_time": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
-      "track": "backend",
-      "github_file_url": "https://github.com/mashm3ll0w/hngx_stage_1/blob/main/hngx_stage_1/views.py",
-      "github_repo_url": "https://github.com/mashm3ll0w/hngx_stage_1",
-      "status_code": HttpResponse.status_code
-    }
-      return JsonResponse(data, safe=False)
-    else:
-      return JsonResponse({"Error": "Wrong User/Track"}, safe=False)
-  else:
-    return JsonResponse({"Error": "Please use a GET request"}, safe=False)
+  return JsonResponse({"greeting": "How are you doing?"}, safe=False)
