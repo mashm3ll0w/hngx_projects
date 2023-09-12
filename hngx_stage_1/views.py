7f51785b1cd8a67394eb0a341f0862146180ab83
Charles Swaleh <charlswall@live.com>
2023-09-12 14:04:33 +0300

    created the routes to view all and a single person

--- hngx_stage_1/views.py
+++ hngx_stage_1/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from http import HTTPStatus
 from django.http import HttpResponse, JsonResponse
 from .models import Person
 # Create your views here.
 
 def index(request):
-  return JsonResponse({"greeting": "How are you doing?"}, safe=False)
+  if request.method == "GET":
+    names = Person.objects.all()
+    persons = [person.serialize() for person in names]
+    return JsonResponse({"persons": persons}, safe=False)
+
+
+def view_user(request, username):
+  if type(username) != str:
+    return JsonResponse({"Error": "Please enter a person's name as a string"}, safe=False)
+
+  username = username.title()
+  try:
+    user = Person.objects.get(name=username)
+    return JsonResponse(user.serialize(), safe=False)
+  except Person.DoesNotExist:
+    return JsonResponse({"Error": "Person not found"}, status=404, safe=False)
